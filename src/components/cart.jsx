8935b21f0181035a1ce73122694c86ec48be31be
Chris Jones <christian_jones_20@yahoo.com>
2024-02-20 23:15:31 +0200

    created home page. maybe need few more tweaks

---
+++ src/components/cart.jsx
@@ -0,0 +1,13 @@
+
+
+function Cart() {
+    return (
+        <>
+        This will be the cart
+        
+        
+        </>
+    )
+};
+
+export default Cart;
---
+++ src/components/dresses.jsx
@@ -0,0 +1,13 @@
+
+function Dresses() {
+    return (
+        <div className="container">
+            <div className="allDresses">
+                <h1> All Dresses </h1>
+
+            </div>
+        </div>
+    )
+};
+
+export default Dresses;
